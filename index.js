dfc09e5fea46e86f4bcf3c537d9d0e95711f7c28
Ashik Ali Shanto <shanto902@gmail.com>
2023-06-10 03:00:39 +0600

    Mongodb authentication completed

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
+// Middlewares
+
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@vista-vocal.6rsob8b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=> {
     res.send('WoW! Its Working')
