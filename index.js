c01fb796c2aa0952badd17b4c282fffb9a991eef
Ashik Ali Shanto <shanto902@gmail.com>
2023-06-12 11:19:16 +0600

    User Api created

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // Middleware
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@vista-vocal.6rsob8b.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,28 +27,70 @@ async function run() {
     await client.connect();
 
     const classCollection = client.db("vocal-vista").collection("classes");
+    const userCollection = client.db("vocal-vista").collection("users");
 
-    app.get('/classes', async(req, res)=> {
-        const result = await classCollection.find().toArray();
-        res.send(result);
+    // User Related APIs
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find({}).toArray();
+      res.send(users);
     })
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'User Exist'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    app.patch('/users/admin/:id', async (req,res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result)
+    })
+
+    app.patch('/users/instructor/:id', async (req,res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+      },
+    };
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result)
+    })
+
+    // Class related apis
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-  
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res)=> {
-    res.send('WoW! Its Working')
-})
-app.listen(port,()=> {
-    console.log(`VistaVocal is Running on: ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("WoW! Its Working");
+});
+app.listen(port, () => {
+  console.log(`VistaVocal is Running on: ${port}`);
+});
